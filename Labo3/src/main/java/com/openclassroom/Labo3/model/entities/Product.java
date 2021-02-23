1753473cf59161010ec30e3611c0a89661e68412
Quentin Bonaventure <bonaquentin90@gmail.com>
2021-02-23 12:07:16 +0100

    Preparation du projet

---
+++ Labo3/src/main/java/com/openclassroom/Labo3/model/entities/Product.java
@@ -0,0 +1,21 @@
+package com.openclassroom.Labo3.model.entities;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@Entity
+public class Product {
+
+    private int id;
+
+    private String descrption;
+
+    private
+
+}
